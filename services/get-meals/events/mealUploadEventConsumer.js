61ef66539c9895c46e4f3da3774ac0a1b8d54a26
Venkat Gorla <venkatagorla@gmail.com>
2025-05-09 16:24:03 +0530

    [nonfunctional, meal event consumer] initial logic, to be tested - some basic plumbing for the overall logic - added some pending comments to be fixed - current tests passing

--- services/get-meals/events/mealUploadEventConsumer.js
+++ services/get-meals/events/mealUploadEventConsumer.js
@@ -1,3 +1,6 @@
+import { PutItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+
 export async function handler(event) {
   // Lambda entrypoint
   for (const record of event.Records || []) {
@@ -8,4 +11,40 @@ export async function handler(event) {
 
 export async function handleMealUploadedEvent(event) {
   // business logic, write to DynamoDB
+  const ddb = new DynamoDBClient({ region: "us-east-1" }); // vegorla use shared const
+  const TABLE_NAME = process.env.MEALS_TABLE;
+  const BUCKET = process.env.BUCKET_NAME;
+
+  // vegorla this object creation can be moved to utility function and unit tested
+  const {
+    mealId,
+    userId,
+    title,
+    description,
+    imageKey,
+    createdAt = new Date().toISOString(),
+  } = event.detail;
+
+  // vegorla how to remove/ hide s3 bucket dependency? This should not be given to the client
+  const imageUrl = `https://${BUCKET}.s3.amazonaws.com/${imageKey}`;
+
+  const item = {
+    mealId,
+    userId,
+    title,
+    description,
+    createdAt,
+    likes: 0,
+    imageUrl,
+  };
+
+  const command = new PutItemCommand({
+    TableName: TABLE_NAME,
+    Item: marshall(item),
+  });
+
+  await ddb.send(command);
+  // vegorla try catch handling similar to upload
+  // return response that can be validated? Note: this is an internal Lambda
+  console.log("Meal inserted into MealsRead table:", mealId);
 }
