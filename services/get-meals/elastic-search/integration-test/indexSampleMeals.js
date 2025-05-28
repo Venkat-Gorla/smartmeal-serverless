474e2c07f1e3c7c5c3f72f243a4e5cc09f5e6776
Venkat Gorla <venkatagorla@gmail.com>
2025-05-28 12:03:11 +0530

    [es integration test improvement] to create multiple documents and verify pagination works - all good

--- services/get-meals/elastic-search/integration-test/indexSampleMeals.js
+++ services/get-meals/elastic-search/integration-test/indexSampleMeals.js
@@ -7,20 +7,51 @@
 import { indexMeal } from "../indexer.js";
 import { getMeals } from "../query.js";
 
-const sampleMeal = {
-  mealId: "meal-int-001",
-  userId: "user-int",
-  title: "Test Meal",
-  description: "This is an integration test meal",
-  createdAt: new Date().toISOString(),
-  likes: 0,
-  imageUrl: "https://should-not-be-indexed.com/img.jpg", // ignored
-};
+const userId = "user-int";
+const now = new Date();
 
 (async () => {
-  await indexMeal(sampleMeal);
-  console.log("Indexed!");
-
-  const result = await getMeals({ userId: "user-int" });
-  console.log("Queried:", JSON.stringify(result, null, 2));
+  await indexMeals();
+  await queryMeals();
 })();
+
+async function indexMeals() {
+  // Index 4 meals with incremental createdAt and unique IDs/titles
+  const totalMeals = 4;
+  for (let i = 1; i <= totalMeals; i++) {
+    await indexMeal({
+      mealId: `meal-${i}`,
+      userId,
+      title: `title-${i}`,
+      description: `desc-${i}`,
+      createdAt: new Date(now.getTime() + i * 1000).toISOString(),
+      likes: 0,
+      imageUrl: `https://skip.com/${i}.jpg`, // ignored
+    });
+  }
+  console.log(`Indexed ${totalMeals} meals`);
+}
+
+async function queryMeals() {
+  const pageSize = 2;
+  const allPages = [];
+  let page = 1;
+  let currentPage;
+
+  do {
+    currentPage = await getMeals({ userId, page, pageSize });
+    allPages.push(currentPage);
+    page++;
+  } while (currentPage.hasNext);
+
+  console.log("Queried all pages:", JSON.stringify(allPages, null, 2));
+
+  const [page1, page2] = allPages;
+
+  // there might be some other meals created by the same user, so not doing
+  // lots of validation, main purpose of the test is to ensure pagination works.
+  console.assert(page1.meals.length === pageSize, "Expected 2 meals on page 1");
+  console.assert(page1.hasNext === true, "Expected hasNext on page 1");
+  console.assert(page1.hasPrev === false, "Expected hasPrev false on page 1");
+  console.log("\nRequired assertions passed, end of integration test!");
+}
