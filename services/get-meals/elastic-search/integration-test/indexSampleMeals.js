14298aff2b447e218824aef0c0e021162e51bf98
Venkat Gorla <venkatagorla@gmail.com>
2025-05-28 21:51:37 +0530

    [es integration test] clean code improvements - use small focused functions and simplify the flow - test passes

--- services/get-meals/elastic-search/integration-test/indexSampleMeals.js
+++ services/get-meals/elastic-search/integration-test/indexSampleMeals.js
@@ -33,25 +33,40 @@ async function indexMeals() {
 }
 
 async function queryMeals() {
-  const pageSize = 2;
-  const allPages = [];
   let page = 1;
+  const pageSize = 2;
   let currentPage;
+  let page1 = null;
 
   do {
     currentPage = await getMeals({ userId, page, pageSize });
-    allPages.push(currentPage);
+    processCurrentPage(currentPage);
+
+    if (page === 1) {
+      page1 = currentPage;
+    }
     page++;
   } while (currentPage.hasNext);
 
-  console.log("Queried all pages:", JSON.stringify(allPages, null, 2));
+  validateFirstPage(page1, pageSize);
+  console.log("\nRequired assertions passed, end of integration test!");
+}
 
-  const [page1, page2] = allPages;
+function processCurrentPage(currentPage) {
+  console.log(
+    `\nPrinting page number ${currentPage.page}:`,
+    JSON.stringify(currentPage, null, 2)
+  );
+}
 
+function validateFirstPage(page1, pageSize) {
   // there might be some other meals created by the same user, so not doing
   // lots of validation, main purpose of the test is to ensure pagination works.
-  console.assert(page1.meals.length === pageSize, "Expected 2 meals on page 1");
-  console.assert(page1.hasNext === true, "Expected hasNext on page 1");
-  console.assert(page1.hasPrev === false, "Expected hasPrev false on page 1");
-  console.log("\nRequired assertions passed, end of integration test!");
+  assert(page1.meals.length === pageSize, "Expected 2 meals on page 1");
+  assert(page1.hasNext === true, "Expected hasNext on page 1");
+  assert(page1.hasPrev === false, "Expected hasPrev false on page 1");
+}
+
+function assert(condition, message) {
+  if (!condition) throw new Error(message);
 }
