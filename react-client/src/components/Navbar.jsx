38a1319e14d90d478585827ada51ac21012d986f
Venkat Gorla <venkatagorla@gmail.com>
2025-06-06 12:15:37 +0530

    [react client] responsive Navbar and style fixes - when screen size is small, collapse Navbar into a Hamburger menu - highlight current path selection: Home, Browse, About etc.

--- react-client/src/components/Navbar.jsx
+++ react-client/src/components/Navbar.jsx
@@ -1,28 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
   const { isAuthenticated, logout } = useAuth();
+  const location = useLocation();
+  const currentPathStyle = "fw-bold text-light border-bottom";
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 fixed-top">
       <Link to="/" className="navbar-brand text-primary">
         Smart Meals
       </Link>
-      <div className="collapse navbar-collapse">
+
+      {/* Mobile toggle button, add js reference to make it work */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav me-auto">
           <li className="nav-item">
-            <Link to="/" className="nav-link">
+            <Link
+              to="/"
+              className={`nav-link ${
+                location.pathname === "/" ? currentPathStyle : ""
+              }`}
+            >
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/browse" className="nav-link">
+            <Link
+              to="/browse"
+              className={`nav-link ${
+                location.pathname === "/browse" ? currentPathStyle : ""
+              }`}
+            >
               Browse
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link">
+            <Link
+              to="/about"
+              className={`nav-link ${
+                location.pathname === "/about" ? currentPathStyle : ""
+              }`}
+            >
               About
             </Link>
           </li>
