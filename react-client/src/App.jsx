72af89f3383fec26b6fa106cd972f518d2c0949c
Venkat Gorla <venkatagorla@gmail.com>
2025-06-12 10:57:09 +0530

    [react client] refactor: extract route definitions into AppRoutes.jsx for better modularity - Moved all <Route> configuration into AppRoutes.jsx - App now focuses only on composition (context + router)

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,37 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Browse from "./pages/Browse";
-import Profile from "./pages/Profile";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./routes/ProtectedRoute";
+import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from "./AppRoutes";
 
 export default function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/browse" element={<Browse />} />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <AppRoutes />
       </Router>
     </AuthProvider>
   );
---
+++ react-client/src/AppRoutes.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Browse from "./pages/Browse";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
