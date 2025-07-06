87c2101fd6721c18095a739f96aa992970e0dcea
Venkat Gorla <venkatagorla@gmail.com>
2025-07-06 15:47:31 +0530

    [react client] style(about): add horizontal rule to separate intro from features

--- react-client/src/pages/About.jsx
+++ react-client/src/pages/About.jsx
@@ -67,6 +67,8 @@ export default function About() {
         Bootstrap.
       </p>
 
+      <hr className="mb-4" />
+
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
         {features.map((feature, index) => (
           <div key={index} className="col text-center">
